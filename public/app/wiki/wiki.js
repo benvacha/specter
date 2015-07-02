7821eb305cff9ad6eb5e99985ab3252259e49159
benvacha <ben@benvacha.com>
2015-07-02 14:33:25 -0600

    Add wiki edit style. Combine wiki control and content views

--- public/app/wiki/wiki.js
+++ public/app/wiki/wiki.js
@@ -19,53 +19,60 @@ angular.module('app.wiki', [
             data: { requireUser: true },
             templateUrl: 'specter/app/wiki/wiki.html'
         })
-        .state('wiki.page', {
+        .state('wiki.show', {
             url: '/{path:nonURIEncoded}',
             params: { path: '' },
-            views: {
-                'control': {
-                    templateUrl: 'specter/app/wiki/wiki.control.show.html',
-                    controller: ['$location', '$scope', '$state', '$stateParams', function($location, $scope, $state, $stateParams) {
-                        $scope.path = $stateParams.path ? $stateParams.path : '/';
-                        $scope.go = function() {
-                            $location.path($scope.path);
-                        };
-                        $scope.edit = function() {
-                            $state.go('wiki.page.edit', $stateParams);
-                        };
-                    }]
-                },
-                'content': {
-                    templateUrl: 'specter/app/wiki/wiki.content.show.html',
-                    controller: ['$scope', '$state', '$stateParams', function($scope, $state, $stateParams) {
-                        var path = $stateParams.path ? $stateParams.path : '/';
-                            date = new Date(),
-                            day = date.getDate(),
-                            month = date.getMonth() + 1,
-                            year = date.getFullYear();
-                        $scope.path = path;
-                        $scope.date = day + '/' + month + '/' + year;
-                    }]
-                }
-            }
+            templateUrl: 'specter/app/wiki/wiki.show.html',
+            controller: ['$location', '$scope', '$state', '$stateParams', function($location, $scope, $state, $stateParams) {
+                var path = $stateParams.path ? $stateParams.path : '/';
+                    date = new Date(),
+                    day = date.getDate(),
+                    month = date.getMonth() + 1,
+                    year = date.getFullYear();
+                $scope.path = path;
+                $scope.date = day + '/' + month + '/' + year;
+                $scope.go = function() {
+                    $location.path($scope.path);
+                };
+                $scope.edit = function() {
+                    $state.go('wiki.edit', $stateParams);
+                };
+            }]
         })
-        .state('wiki.page.edit', {
+        .state('wiki.edit', {
             params: { path: '' },
-            views: {
-                'control@wiki': {
-                    templateUrl: 'specter/app/wiki/wiki.control.edit.html',
-                    controller: ['$scope', '$state', '$stateParams', function($scope, $state, $stateParams) {
-                        $scope.save = function() {
-                            $state.go('wiki.page', $stateParams);
-                        };
-                    }]
-                },
-                'content@wiki': {
-                    templateUrl: 'specter/app/wiki/wiki.content.edit.html',
-                    controller: ['$scope', '$state', '$stateParams', function($scope, $state, $stateParams) {
-                        $scope.path = $stateParams.path;
-                    }]
+            templateUrl: 'specter/app/wiki/wiki.edit.html',
+            controller: ['$window', '$scope', '$state', '$stateParams', function($window, $scope, $state, $stateParams) {
+                $scope.path = $stateParams.path;
+                if($window.innerWidth <= 550) {
+                    $scope.showMarkdown = true;
+                    $scope.showPreview = false;
+                } else {
+                    $scope.showMarkdown = true;
+                    $scope.showPreview = true;
                 }
-            }
+                $scope.toggleMarkdown = function() {
+                    $scope.showMarkdown = !$scope.showMarkdown;
+                    if(!$scope.showMarkdown && !$scope.showPreview) {
+                        $scope.showPreview = true;
+                    } else if($scope.showMarkdown && $window.innerWidth <= 550) {
+                        $scope.showPreview = false;
+                    }
+                };
+                $scope.togglePreview = function() {
+                    $scope.showPreview = !$scope.showPreview;
+                    if(!$scope.showPreview && !$scope.showMarkdown) {
+                        $scope.showMarkdown = true;
+                    } else if($scope.showPreview && $window.innerWidth <= 550) {
+                        $scope.showMarkdown = false;
+                    }
+                };
+                $scope.cancel = function() {
+                    $state.go('wiki.show', $stateParams);
+                };
+                $scope.save = function() {
+                    $state.go('wiki.show', $stateParams);
+                };
+            }]
         });
 }]);

103dc1ded61cb487a0abdc002ec7a873a91c7cbd
benvacha <ben@benvacha.com>
2015-07-24 16:39:41 -0600

    Add functional and stylized wiki show

--- publics/modules/wiki.js
+++ publics/modules/wiki.js
@@ -3,7 +3,8 @@
 //
 //
 angular.module('app.wiki', [
-    'ui.router'
+    'ui.router',
+    'hc.marked'
 ])
 
 //
@@ -19,6 +20,11 @@ angular.module('app.wiki', [
 //
 //
 .config(['$stateProvider', function($stateProvider) {
+    var formatDatetime = function(epoch) {
+        var date = new Date(epoch);
+        return (date.getMonth()+1) + '/' + date.getDate() + '/'
+            + date.getFullYear() + ' ' + date.getHours() + ':' + date.getMinutes();
+    };
     $stateProvider
         .state('wiki', {
             data: { private:true },
@@ -27,6 +33,38 @@ angular.module('app.wiki', [
         .state('wiki.show', {
             url: '/{path:nonURIEncoded}',
             params: { path:'' },
-            templateUrl: 'specter/views/wiki.show.html'
+            templateUrl: 'specter/views/wiki.show.html',
+            controller: ['$http','$location','$scope','$state','$stateParams', 
+                        function($http,$location,$scope,$state,$stateParams) {
+                //
+                $scope.path = $stateParams.path || '/';
+                $http.get('/specter/apis/pages/' + encodeURIComponent($scope.path))
+                    .success(function(data, status, headers, config) {
+                        $scope.markdown = data.markdown;
+                        $scope.created = formatDatetime(data.created);
+                        $scope.updated = formatDatetime(data.updated);
+                    })
+                    .error(function(data, status, headers, config) {
+                        $scope.markdown = 'Server Error';
+                    });
+                //
+                $scope.toggleMenu = function() {
+                    $scope.showMenu = !$scope.showMenu;
+                }
+                $scope.toggleAddress = function() {
+                    $scope.showAddress = !$scope.showAddress;
+                }
+                //
+                $scope.go = function(path) {
+                    $location.path(path);
+                }
+                $scope.edit = function() {
+                    $state.go('wiki.edit', $stateParams);
+                }
+            }]
         })
+        .state('wiki.edit', {
+            params: { path:'' },
+            templateUrl: 'specter/views/wiki.edit.html'
+        });
 }]);
